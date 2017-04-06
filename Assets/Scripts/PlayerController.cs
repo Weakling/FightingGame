e963d1a9eb5690e95161ae3a9b2334e746e23845
Weakling <mullen13@purdue.edu>
2017-04-06 10:23:55 -0400

    Ug

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+    public float error;
+
+    private float moveVelocity;
+    public float xVelocity, yVelocity;
+    public float moveSpeed;
+
+
+    private string inputAxisX, inputAxisY;
+
+    private Rigidbody2D myRigidbody2D;
+
+	void Start ()
+    {
+        inputAxisX = "HorizontalP1";
+        inputAxisY = "VerticalP1";
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        error = 0;
+    }
+	
+	void Update ()
+    {
+        Move();
+
+        //xVelocity = 0;
+        //yVelocity = 0;
+        //myRigidbody2D.velocity = new Vector2(0f, myRigidbody2D.velocity.y);
+	}
+
+
+
+    private void Move()
+    {
+        xVelocity = Input.GetAxisRaw(inputAxisX);
+        yVelocity = Input.GetAxisRaw(inputAxisY);
+
+        myRigidbody2D.velocity = new Vector2(xVelocity * moveSpeed, myRigidbody2D.velocity.y);
+        // right..
+        /*if(xVelocity > error)
+        {
+            myRigidbody2D.velocity = new Vector2(moveSpeed, myRigidbody2D.velocity.y);
+        }
+        // left..
+        if (xVelocity < -error)
+        {
+            myRigidbody2D.velocity = new Vector2(-moveSpeed, myRigidbody2D.velocity.y);
+        }*/
+    }
+}
